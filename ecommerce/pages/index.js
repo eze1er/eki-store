c8feb674fcc959780c528cbf90e01a50ea74b78b
Ezechiel ITIMBIEN KAKANA <papaezechiel@gmail.com>
2022-07-05 21:58:12 -0400

    banner done

--- ecommerce/pages/index.js
+++ ecommerce/pages/index.js
@@ -1,20 +1,34 @@
 import React from "react";
 
-const Home = () => {
+import { client } from "../lib/client";
+import { Product, FooterBanner, HeroBanner } from "../components";
+const Home = ({ products, bannerData}) => {
   return (
-    <>
-      heroBanner
-      <div>
+    <div>
+      <HeroBanner heroBanner={bannerData.length && bannerData[0]} />
+      <div className="products-heading">
         <h1>Best Selling Product</h1>
         <p>Speakers of many variations</p>
       </div>
-      <div>
-        {["product01 ", "product02 ", "product03 "].map((product) => product)}
+      <div className="products-container">
+        {products?.map((product) => product.name)}
       </div>
-
-      footer
-    </>
+      <FooterBanner />
+    </div>
   );
 };
+
+export const getServerSideProps = async () => {
+  const query = '*[_type == "product"]';
+  const products = await client.fetch(query);
+
+  const bannerQuery = '*[_type == "banner"]';
+  const bannerData = await client.fetch(bannerQuery);
+
+  return {
+    props: { products, bannerData },
+  };
+};
+
 
 export default Home;
