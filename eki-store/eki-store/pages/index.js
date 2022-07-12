901720e69f9f6205d3de817b12e2993c6d276ced
Ezechiel ITIMBIEN KAKANA <papaezechiel@gmail.com>
2022-07-11 21:50:22 -0400

    new file using sanity w3

---
+++ eki-store/eki-store/pages/index.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div>
+      HeroBanner
+      <div className="products-heading">
+        <h2>Best Selling Products</h2>
+        <p>Speakers of many variations</p>
+      </div>
+      <div className="products-container">
+        {["prod01 ", "prod02 "].map((product) => product)}
+      </div>
+      Footer
+    </div>
+  );
+};
+
+export default Home;
