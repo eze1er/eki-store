07e8274ccdb0a79a36cf8d0e1d31465754e03052
Ezechiel ITIMBIEN KAKANA <papaezechiel@gmail.com>
2022-07-12 04:10:52 -0400

    data fetch end paint

--- eki-store/eki-store/pages/index.js
+++ eki-store/eki-store/pages/index.js
@@ -9,13 +9,13 @@ import { HeroBanner, FooterBanner, Product } from "../components";
 const Home = ({ products, bannerData }) => {
   return (
     <div>
-      <HeroBanner />
+      <HeroBanner heroBanner={bannerData.length && bannerData[0]} />
       <div className="products-heading">
         <h2>Best Selling Products</h2>
         <p>Speakers of many variations</p>
       </div>
       <div className="products-container">
-        {["prod01 ", "prod02 ", "product3 "].map((product) => product)}
+        {products?.map((product) => product.name)}
       </div>
       <FooterBanner />
     </div>
