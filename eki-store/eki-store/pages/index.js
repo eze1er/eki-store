3cdd797762795634a78e168f516e7b600137098d
Ezechiel ITIMBIEN KAKANA <papaezechiel@gmail.com>
2022-07-11 21:56:10 -0400

    a product added

--- eki-store/eki-store/pages/index.js
+++ eki-store/eki-store/pages/index.js
@@ -9,7 +9,7 @@ const Home = () => {
         <p>Speakers of many variations</p>
       </div>
       <div className="products-container">
-        {["prod01 ", "prod02 "].map((product) => product)}
+        {["prod01 ", "prod02, product3 "].map((product) => product)}
       </div>
       Footer
     </div>
